f8f95e6359f71b5380f2c3cea0ce5d9a0ab643e9
Abdur Rehman <abdurrehman112919@gmail.com>
2026-08-09 11:14:38 +0500

    Added noreply email

--- lib/email.ts
+++ lib/email.ts
@@ -11,7 +11,7 @@ import { siteConfig } from "@/data/siteConfig";
  * "Pixel Nox <notifications@pixelnox.com>" so you can send to any inbox,
  * including your real siteConfig.email.
  */
-const FROM_ADDRESS = "Pixel Nox <hello@pixelnox.site>";
+const FROM_ADDRESS = "Pixel Nox <noreply@pixelnox.site>";
 
 let resendClient: Resend | null = null;
 
